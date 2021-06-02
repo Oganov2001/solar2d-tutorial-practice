1ae568561c6096e9b6adc93f7c138cd7559caeba
oganov2001 <georgeoganov@hotmail.com>
2021-06-02 17:35:52 +0300

    Corrected main.lua

--- main.lua
+++ main.lua
@@ -4,21 +4,19 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Setup
 local platform = display.newRect( 0, 440, 480, 50 )
 local balloon = display.newCircle( 160, 160, 50 )
 
--- Initialization
 local physics = require('physics')
 
--- Update
+
 physics.start()
 physics.addBody( platform, "static")
 physics.addBody( balloon, "dynamic", 0.6)
 
--- Functions
+
 local function bounce()
     balloon:applyLinearImpulse(0, -0.71, balloon.x, balloon.y)
 end
--- Events
+
 balloon:addEventListener("tap", bounce)
